e2bd459d99b9c6a7c59d4f99cf82bbacb3f57fd6
Tim <Tim-Knappe@gmx.de>
2024-08-22 00:10:28 +0200

    Adjusted descriptions and Titles

--- src/vs/workbench/contrib/welcomeGettingStarted/common/gettingStartedContent.ts
+++ src/vs/workbench/contrib/welcomeGettingStarted/common/gettingStartedContent.ts
@@ -225,15 +225,15 @@ export const walkthroughs: GettingStartedWalkthroughContent = [
 	{
 		id: 'pearAISetupOptions',
 		isFeatured: false,
-		title: localize('gettingStarted.pearAISetup.title', "Finish your Setup"),
+		title: localize('gettingStarted.pearAISetup.title', "Configure Your Optional Settings"),
 		icon: setupIcon, // replace Icon
-		description: localize('gettingStarted.pearAISetup.description', "Finsih your Setup and Improve your Experience by Customizing your Settings"),
+		description: localize('gettingStarted.pearAISetup.description', "Finish your setup and improve your experience by customizing your settings"),
 		content: {
 			type: 'steps',
 			steps: [
 				{
 					id: 'setPath',
-					title: localize('gettingStarted.pearAISetup.path.title', "Add Pear to your PATH"),
+					title: localize('gettingStarted.pearAISetup.path.title', "Add Pear to Your PATH"),
 					description: localize('gettingStarted.pearAISetup.path.description', "This allows CLI use and many more features\n{0}", Button(localize('addToPath', "Do it for me"), 'command:workbench.action.pearAI.runAddPearToPath')),
 					media: {
 						type: 'svg', altText: 'VS Code extension marketplace with featured language extensions', path: 'extensions.svg' // replace media
@@ -241,8 +241,8 @@ export const walkthroughs: GettingStartedWalkthroughContent = [
 				},
 				{
 					id: 'desktopShortcut',
-					title: localize('gettingStarted.pearAISetup.desktopShortcut.title', "Enable Context menu shortcuts"),
-					description: localize('gettingStarted.pearAISetup.desktopShortcut.description', "Quickly open files from context menu with PearAI\n{0}", Button(localize('addShortcut', "Add desktop shortcut"), 'command:workbench.action.pearAI.runAddContextMenuShortcut')),
+					title: localize('gettingStarted.pearAISetup.desktopShortcut.title', "Enable Context Menu Shortcuts"),
+					description: localize('gettingStarted.pearAISetup.desktopShortcut.description', "Quickly open files from the context menu with PearAI\n{0}", Button(localize('addShortcut', "Add Desktop Shortcut"), 'command:workbench.action.pearAI.runAddContextMenuShortcut')),
 					media: {
 						type: 'svg', altText: 'Integrated terminal running a few npm commands', path: 'terminal.svg' // replace media
 					},
@@ -250,7 +250,7 @@ export const walkthroughs: GettingStartedWalkthroughContent = [
 				{ // this should only be shown if a VSCode installation existed before
 					id: 'replaceVSCode',
 					title: localize('gettingStarted.pearAISetup.replaceVSCode.title', "Replace VSCode with Pear"),
-					description: localize('gettingStarted.pearAISetup.replaceVSCode.description', "We detected that you were using VSCode, do you want us to replace VSCode with Pear?\n{0}", Button(localize('replaceVS', "Replace"), 'command:workbench.action.pearAI.runReplaceVSCode')),
+					description: localize('gettingStarted.pearAISetup.replaceVSCode.description', "We detected that you were using VSCode. Do you want us to replace VSCode with Pear?\n{0}", Button(localize('replaceVS', "Replace"), 'command:workbench.action.pearAI.runReplaceVSCode')),
 					media: {
 						type: 'svg', altText: 'Integrated terminal running a few npm commands', path: 'terminal.svg' // replace media
 					},
